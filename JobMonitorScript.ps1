ba44d20246217aa625a5a8976bf7fe71103fa60a
Aleksei Rjeutski <aleksei.rj@polontech.biz>
2020-05-07 11:41:05 +0300

    Updated how the DT module is loaded.

--- JobMonitorScript.ps1
+++ JobMonitorScript.ps1
@@ -3,7 +3,8 @@
 Try {
     # Import the Carbonite PowerShell module
     # This may be \Service\ or \Console\ depending on your installation
-    Import-Module "$PSScriptRoot\DoubleTake.PowerShell.dll" -Force
+    [Reflection.Assembly]::LoadFile("$PSScriptRoot\DoubleTake.PowerShell.dll") | out-null
+    
     # Import 'Set-D42DeviceStatus' script
     Import-Module -Name ($PSScriptRoot + "\SetDeviceStatus.ps1") -Force
 
